6e843fed2c8e9dc0181b63cce22eef8b9d3c9236
ArchAngel776 <karol.marciniak155@gmail.com>
2026-08-15 00:14:06 +0200

    Remove unused "compare" method from the HSDH nn.module.

--- app/modules/HSDH.pyi
+++ app/modules/HSDH.pyi
@@ -1,4 +1,4 @@
-from torch import Tensor, no_grad
+from torch import Tensor
 from torch.nn import Module, Sequential
 from typing import Self
 from app.modules.HashGenerator import HashGenerator
@@ -12,6 +12,3 @@ class HSDH(Module):
     def __init__(self: Self, hash_length: int, alpha: float) -> None: ...
 
     def forward(self: Self, image_i: Tensor, image_j: Tensor, target: Tensor) -> Tensor: ...
-
-    @no_grad()
-    def compare(self: Self, image_i: Tensor, image_j: Tensor) -> Tensor: ...
